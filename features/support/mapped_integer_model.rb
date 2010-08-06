13a8ad30062aaf7cec8f42e81b0807a64b98f92b
Greg Spurrier <greg.spurrier@zvents.com>
2010-08-06 13:01:31 -0700

    Go go gadet metaprogramming
    Implement has_enumeration and remove hard-coded example.

--- features/support/mapped_integer_model.rb
+++ features/support/mapped_integer_model.rb
@@ -1,38 +1,3 @@
 class MappedIntegerModel < ActiveRecord::Base
-  #has_enumeration :color, :red => 1, :green => 2, :blue => 3
-
-  class ColorIntegerMap
-    attr_reader :raw_value
-
-    MAPPING = {:red => 1, :green => 2, :blue => 3}
-
-    def initialize(value)
-      @raw_value = value
-    end
-
-    def value
-      MAPPING.invert[@raw_value]
-    end
-
-    def self.from_sym(sym)
-      new(MAPPING[sym])
-    end
-
-    def red?
-      @raw_value == 1
-    end
-    
-    def green?
-      @raw_value == 2
-    end
-
-    def blue?
-      @raw_value == 3
-    end
-  end
-
-  composed_of :color,
-    :class_name => 'MappedIntegerModel::ColorIntegerMap',
-    :mapping => %w(color raw_value),
-    :converter => :from_sym
+  has_enumeration :color, :red => 1, :green => 2, :blue => 3
 end
--- lib/has_enumeration/class_methods.rb
+++ lib/has_enumeration/class_methods.rb
@@ -1,7 +1,49 @@
 module HasEnumeration
   module ClassMethods
-    def has_enumeration(name, mapping)
-        
+    def has_enumeration(attribute, mapping)
+      # ActiveRecord's composed_of method will do most of the work for us.
+      # All we have to do is cons up a class that implements the bidirectional
+      # mapping described by the provided hash.
+      klass = create_enumeration_mapping_class(mapping)
+
+      # Bind the class to a name within the scope of this class
+      attribute_name = attribute.to_s
+      mapping_class_name = attribute_name.camelize
+      const_set(mapping_class_name, klass)
+      scoped_class_name = [self.name, mapping_class_name].join('::')
+
+      composed_of(attribute,
+        :class_name => scoped_class_name,
+        :mapping => [attribute_name, 'raw_value'],
+        :converter => :from_sym
+      )
+    end
+
+  private
+    def create_enumeration_mapping_class(mapping)
+      inverted_mapping = mapping.invert
+      Class.new do
+        attr_reader :raw_value, :value
+
+        define_method :initialize do |raw_value|
+          @raw_value = raw_value
+          @value = inverted_mapping[raw_value]
+        end
+
+        mapping.keys.each do |sym|
+          predicate = "#{sym}?".to_sym
+          value = mapping[sym]
+          define_method predicate do
+            @raw_value == value 
+          end
+        end
+
+        (class <<self;self;end).class_eval do
+          define_method :from_sym do |sym|
+            new(mapping[sym])
+          end
+        end
+      end
     end
   end
 end
